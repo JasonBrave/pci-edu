867a6caecfd94c238d7e1fb6be9cf309294401fe
Jason Xu <40355221+JasonBrave@users.noreply.github.com>
2023-05-02 08:40:41 -0400

    Update testbench

--- tb/sim_main.cpp
+++ tb/sim_main.cpp
@@ -25,222 +25,188 @@
 
 void tb_pci_reset(Vedu_verilator_wrapper *vedu, VerilatedVcdC *tfp) {
 	Verilated::timeInc(1);
-	vedu->frame = 1;
-	vedu->ad_in_en = 0;
-	vedu->cbe_in_en = 0;
+	vedu->frame_in = 1;
 	vedu->idsel = 0;
-	vedu->irdy = 1;
-	vedu->rst = 1;
-	vedu->clk = 0;
-	vedu->eval();
-	tfp->dump(Verilated::time());
-
-	Verilated::timeInc(1);
-	vedu->frame = 1;
-	vedu->ad_in_en = 0;
-	vedu->cbe_in_en = 0;
-	vedu->idsel = 0;
-	vedu->irdy = 1;
+	vedu->irdy_in = 1;
 	vedu->rst = 0;
 	vedu->clk = 1;
 	vedu->eval();
 	tfp->dump(Verilated::time());
 
 	Verilated::timeInc(1);
-	vedu->frame = 1;
-	vedu->ad_in_en = 0;
-	vedu->cbe_in_en = 0;
+	vedu->frame_in = 1;
 	vedu->idsel = 0;
-	vedu->irdy = 1;
+	vedu->irdy_in = 1;
 	vedu->rst = 0;
 	vedu->clk = 0;
 	vedu->eval();
 	tfp->dump(Verilated::time());
 
 	Verilated::timeInc(1);
-	vedu->frame = 1;
-	vedu->ad_in_en = 0;
-	vedu->cbe_in_en = 0;
+	vedu->frame_in = 1;
 	vedu->idsel = 0;
-	vedu->irdy = 1;
+	vedu->irdy_in = 1;
 	vedu->rst = 0;
 	vedu->clk = 1;
+	vedu->eval();
+	tfp->dump(Verilated::time());
+
+	Verilated::timeInc(1);
+	vedu->frame_in = 1;
+	vedu->idsel = 0;
+	vedu->irdy_in = 1;
+	vedu->rst = 0;
+	vedu->clk = 0;
 	vedu->eval();
 	tfp->dump(Verilated::time());
 
 	vedu->rst = 1;
 }
 
-void tb_pci_idle(Vedu_verilator_wrapper *vedu, VerilatedVcdC *tfp, unsigned int n) {
+void tb_pci_idle(Vedu_verilator_wrapper *vedu, VerilatedVcdC *tfp,
+				 unsigned int n) {
 	for (unsigned int i = 0; i < n; i++) {
 		Verilated::timeInc(1);
-		vedu->frame = 1;
-		vedu->ad_in_en = 0;
+		vedu->frame_in = 1;
 		vedu->idsel = 0;
-		vedu->cbe_in_en = 0;
-		vedu->irdy = 1;
-		vedu->clk = 0;
+		vedu->irdy_in = 1;
+		vedu->clk = 1;
 		vedu->eval();
 		tfp->dump(Verilated::time());
 
 		Verilated::timeInc(1);
-		vedu->frame = 1;
-		vedu->ad_in_en = 0;
+		vedu->frame_in = 1;
 		vedu->idsel = 0;
-		vedu->cbe_in_en = 0;
-		vedu->irdy = 1;
-		vedu->clk = 1;
+		vedu->irdy_in = 1;
+		vedu->clk = 0;
 		vedu->eval();
 		tfp->dump(Verilated::time());
 	}
 }
 
-uint32_t tb_pci_config_read32(Vedu_verilator_wrapper *vedu, VerilatedVcdC *tfp, unsigned int reg) {
+uint32_t tb_pci_config_read32(Vedu_verilator_wrapper *vedu, VerilatedVcdC *tfp,
+							  unsigned int reg) {
 	// AD phase
 	Verilated::timeInc(1);
-	vedu->frame = 0;
+	vedu->frame_in = 0;
 	vedu->ad_in = reg << 2;
-	vedu->ad_in_en = 1;
 	vedu->idsel = 1;
 	vedu->cbe_in = 0xa;
-	vedu->cbe_in_en = 1;
-	vedu->irdy = 1;
-	vedu->clk = 0;
-	vedu->eval();
-	tfp->dump(Verilated::time());
-
-	Verilated::timeInc(1);
-	vedu->frame = 0;
-	vedu->ad_in = reg << 2;
-	vedu->ad_in_en = 1;
-	vedu->idsel = 1;
-	vedu->cbe_in = 0xa;
-	vedu->cbe_in_en = 1;
-	vedu->irdy = 1;
+	vedu->irdy_in = 1;
 	vedu->clk = 1;
 	vedu->eval();
 	tfp->dump(Verilated::time());
 
 	Verilated::timeInc(1);
-	vedu->frame = 1;
-	vedu->ad_in_en = 0;
-	vedu->idsel = 0;
-	vedu->cbe_in = 0xf;
-	vedu->cbe_in_en = 1;
-	vedu->irdy = 0;
+	vedu->frame_in = 0;
+	vedu->ad_in = reg << 2;
+	vedu->idsel = 1;
+	vedu->cbe_in = 0xa;
+	vedu->irdy_in = 1;
 	vedu->clk = 0;
 	vedu->eval();
 	tfp->dump(Verilated::time());
 
 	Verilated::timeInc(1);
-	vedu->frame = 1;
-	vedu->ad_in_en = 0;
+	vedu->frame_in = 1;
 	vedu->idsel = 0;
 	vedu->cbe_in = 0xf;
-	vedu->cbe_in_en = 1;
-	vedu->irdy = 0;
+	vedu->irdy_in = 0;
 	vedu->clk = 1;
+	vedu->eval();
+	tfp->dump(Verilated::time());
+
+	Verilated::timeInc(1);
+	vedu->frame_in = 1;
+	vedu->idsel = 0;
+	vedu->cbe_in = 0xf;
+	vedu->irdy_in = 0;
+	vedu->clk = 0;
 	vedu->eval();
 	tfp->dump(Verilated::time());
 	// C/BE phase and assert IRDY
 	uint32_t value = 0;
 	do {
 		Verilated::timeInc(1);
-		vedu->frame = 1;
-		vedu->ad_in_en = 0;
+		vedu->frame_in = 1;
 		vedu->idsel = 0;
 		vedu->cbe_in = 0xf;
-		vedu->cbe_in_en = 1;
-		vedu->irdy = 0;
-		vedu->clk = 0;
+		vedu->irdy_in = 0;
+		vedu->clk = 1;
 		vedu->eval();
 		tfp->dump(Verilated::time());
 
 		Verilated::timeInc(1);
-		vedu->frame = 1;
-		vedu->ad_in_en = 0;
+		vedu->frame_in = 1;
 		vedu->idsel = 0;
 		vedu->cbe_in = 0xf;
-		vedu->cbe_in_en = 1;
-		vedu->irdy = 0;
-		vedu->clk = 1;
+		vedu->irdy_in = 0;
+		vedu->clk = 0;
 		vedu->eval();
 		tfp->dump(Verilated::time());
 		value = vedu->ad_out;
-	} while (vedu->trdy == 1 && vedu->devsel == 1);
+	} while (vedu->trdy_out == 1 && vedu->devsel_out == 1);
 
 	return value;
 }
 
-void tb_pci_config_write32(Vedu_verilator_wrapper *vedu, VerilatedVcdC *tfp, unsigned int reg,
-						   uint32_t value) {
+void tb_pci_config_write32(Vedu_verilator_wrapper *vedu, VerilatedVcdC *tfp,
+						   unsigned int reg, uint32_t value) {
 	// AD phase
 	Verilated::timeInc(1);
-	vedu->frame = 0;
+	vedu->frame_in = 0;
 	vedu->ad_in = reg << 2;
-	vedu->ad_in_en = 1;
 	vedu->idsel = 1;
 	vedu->cbe_in = 0xb;
-	vedu->cbe_in_en = 1;
-	vedu->irdy = 1;
-	vedu->clk = 0;
-	vedu->eval();
-	tfp->dump(Verilated::time());
-
-	Verilated::timeInc(1);
-	vedu->frame = 0;
-	vedu->ad_in = reg << 2;
-	vedu->ad_in_en = 1;
-	vedu->idsel = 1;
-	vedu->cbe_in = 0xb;
-	vedu->cbe_in_en = 1;
-	vedu->irdy = 1;
+	vedu->irdy_in = 1;
 	vedu->clk = 1;
 	vedu->eval();
 	tfp->dump(Verilated::time());
 
+	Verilated::timeInc(1);
+	vedu->frame_in = 0;
+	vedu->ad_in = reg << 2;
+	vedu->idsel = 1;
+	vedu->cbe_in = 0xb;
+	vedu->irdy_in = 1;
+	vedu->clk = 0;
+	vedu->eval();
+	tfp->dump(Verilated::time());
+
 	vedu->idsel = 0;
-	vedu->frame = 1;
+	vedu->frame_in = 1;
 
 	do {
 		// Data phase
 		Verilated::timeInc(1);
 		vedu->ad_in = value;
-		vedu->ad_in_en = 1;
 		vedu->cbe_in = 0xf;
-		vedu->cbe_in_en = 1;
-		vedu->irdy = 0;
-		vedu->clk = 0;
+		vedu->irdy_in = 0;
+		vedu->clk = 1;
 		vedu->eval();
 		tfp->dump(Verilated::time());
 
 		Verilated::timeInc(1);
 		vedu->ad_in = value;
-		vedu->ad_in_en = 1;
 		vedu->cbe_in = 0xf;
-		vedu->cbe_in_en = 1;
-		vedu->irdy = 0;
-		vedu->clk = 1;
+		vedu->irdy_in = 0;
+		vedu->clk = 0;
 		vedu->eval();
 		tfp->dump(Verilated::time());
-	} while (vedu->trdy == 1 || vedu->devsel == 1);
+	} while (vedu->trdy_out == 1 || vedu->devsel_out == 1);
 
 	// End of transaction
 	Verilated::timeInc(1);
-	vedu->frame = 1;
-	vedu->ad_in_en = 0;
-	vedu->cbe_in_en = 0;
-	vedu->irdy = 1;
-	vedu->clk = 0;
+	vedu->frame_in = 1;
+	vedu->irdy_in = 1;
+	vedu->clk = 1;
 	vedu->eval();
 	tfp->dump(Verilated::time());
 
 	Verilated::timeInc(1);
-	vedu->frame = 1;
-	vedu->ad_in_en = 0;
-	vedu->cbe_in_en = 0;
-	vedu->irdy = 1;
-	vedu->clk = 1;
+	vedu->frame_in = 1;
+	vedu->irdy_in = 1;
+	vedu->clk = 0;
 	vedu->eval();
 	tfp->dump(Verilated::time());
 }
@@ -261,17 +227,22 @@ int main(int argc, char *argv[]) {
 	std::cout << "PCI Configuration Space registers status:" << std::endl;
 	for (int reg = 0; reg < 64; reg++) {
 		uint32_t value = tb_pci_config_read32(vedu, tfp, reg);
-		std::cout << "PCI Cfg Register offset " << reg << " value " << value << std::endl;
+		std::cout << "PCI Cfg Register offset " << reg << " value " << value
+				  << std::endl;
 		tb_pci_idle(vedu, tfp, 5);
 	}
+	/*
 	std::cout << "Write to MSI upper address register" << std::endl;
 	tb_pci_config_write32(vedu, tfp, 0x12, 0x12345678);
+	tb_pci_idle(vedu, tfp, 5);
 	std::cout << "PCI Configuration Space registers status:" << std::endl;
 	for (int reg = 0; reg < 64; reg++) {
 		uint32_t value = tb_pci_config_read32(vedu, tfp, reg);
-		std::cout << "PCI Cfg Register offset " << reg << " value " << value << std::endl;
+		std::cout << "PCI Cfg Register offset " << reg << " value " << value
+				  << std::endl;
 		tb_pci_idle(vedu, tfp, 5);
 	}
-	tb_pci_idle(vedu, tfp, 5);
+	tb_pci_idle(vedu, tfp, 5);*/
+
 	tfp->close();
 }
